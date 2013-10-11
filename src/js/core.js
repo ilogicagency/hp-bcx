96c68a2e4049d17a4212d29ce9e3a5da26d034fe
sandy0201 <sandyl@ilogic.co.za>
2013-10-11 09:26:01 +0200

    timer fixed, sequence time added, styling

--- src/js/core.js
+++ src/js/core.js
@@ -9,39 +9,38 @@
         if (typeof PBAutoCommands == 'undefined')
             console.log('Unable to find Pandoras Box command: "PBAutoCommands"!');
 
-        this.sections = ['video', 'chaos'];
+        this.sections = ['video'];
         this.sectionCurrent = 0;
         this.choices = {
-            green1: {seek: '00:00:10:00'},
-            green2: {seek: '00:00:20:00'},
-            green3: {seek: '00:00:30:00'},
-            green4: {seek: '00:00:40:00'},
-            green5: {seek: '00:00:50:00'},
-            turquoise1: {seek: '00:01:00:00'},
-            turquoise2: {seek: '00:01:20:00'},
-            turquoise3: {seek: '00:01:30:00'},
-            turquoise4: {seek: '00:01:40:00'},
-            turquoise5: {seek: '00:01:50:00'},
-            red1: {seek: '00:02:10:00'},
-            red2: {seek: '00:02:20:00'},
-            red3: {seek: '00:02:30:00'},
-            red4: {seek: '00:02:40:00'},
-            red5: {seek: '00:02:50:00'},
-            blue1: {seek: '00:03:10:00'},
-            blue2: {seek: '00:03:20:00'},
-            blue3: {seek: '00:03:30:00'},
-            blue4: {seek: '00:03:40:00'},
-            blue5: {seek: '00:03:50:00'}
+            green1: {seek: '00:00:00:00', delay: 10000},
+            green2: {seek: '00:01:00:00', delay: 60000},
+            green3: {seek: '00:02:00:00', delay: 60000},
+            green4: {seek: '00:03:00:00', delay: 60000},
+            turquoise1: {seek: '00:00:10:00', delay: 10000},
+            turquoise2: {seek: '00:05:00:00', delay: 60000},
+            turquoise3: {seek: '00:06:00:00', delay: 60000},
+            turquoise4: {seek: '00:07:00:00', delay: 60000},
+            red1: {seek: '00:00:20:00', delay: 10000},
+            red2: {seek: '00:09:00:00', delay: 60000},
+            red3: {seek: '00:10:00:00', delay: 60000},
+            red4: {seek: '00:11:00:00', delay: 60000}
         }
+        this.userChoices = [];
 
         this.queue;
         this.chosen;
+        this.timer;
 
-        this.events();
-        this.reset();
+        this.__construct();
     }
 
     App.prototype = {
+        __construct: function() {
+            this.timer = new Timer();
+
+            this.events();
+            this.reset();
+        },
         events: function() {
             var scope = this;
 
@@ -54,87 +53,85 @@
                 var id = $(this).attr('id');
                 var choice = id;
 
+                scope.userChoices.push(choice);
+
+                var delay = scope.choices[choice].delay;
+
                 scope.sectionCurrent++;
 
-                var seek = scope.choices[choice].seek;
-                var hour = seek.substring(0, 2);
-                var min = seek.substring(3, 5);
-                var sec = seek.substring(6, 8);
-                var frame = seek.substring(9, 11);
+                scope.timer.start(delay, scope);
 
-                if (typeof PBAutoCommands != 'undefined') {
-                    PBAutoCommands.moveSequenceToTime(false, 1, hour, min, sec, frame);
-                    PBAutoCommands.setSequenceTransportMode(false, 1, 'Play');
-                }
-
-                //$('.container').hide();
-
-                //new Timer(index.delay);
+                scope.seek(scope.choices[choice].seek);
 
                 console.log(' - Chosen: ' + choice);
             });
-
-            /*$('#finished .btn').on('click', function() {
-             scope.reset();
-             });*/
         },
-        selection: function() {
-            /*this.queue++;
-             
-             var selection = this.selections[this.queue].selection;
-             var element = $(selection);
-             
-             if (element.length == 0)
-             throw 'Invalid element specified!';
-             
-             $(this.selections[this.queue].selection).show();
-             
-             if (typeof PBAutoCommands != 'undefined') {
-             PBAutoCommands.setSequenceTransportMode(false, 1, 'Pause');
-             }
-             
-             console.log('Choosing: ' + selection);
-             
-             if (this.queue != 1) {
-             console.log('------------------------');
-             console.log('User music: ' + this.chosen.music);
-             console.log('------------------------');
-             }*/
+        seek: function(seek) {
+            var hour = seek.substring(0, 2);
+            var min = seek.substring(3, 5);
+            var sec = seek.substring(6, 8);
+            var frame = seek.substring(9, 11);
+
+            if (typeof PBAutoCommands != 'undefined') {
+                PBAutoCommands.moveSequenceToTime(false, 1, hour, min, sec, frame);
+                PBAutoCommands.setSequenceTransportMode(false, 1, 'Play');
+            }
+        },
+        selection: function(scope) {
+            $('#' + scope.userChoices[scope.sectionCurrent - 1]).removeClass('active');
+
+            var playBack = scope.random(scope.choices);;
+
+            scope.seek(scope.choices[playBack].seek);
+        },
+        random: function(obj) {
+            var random = Math.floor(Math.random() * this.objLength(obj));
+            var count = 0;
+
+            for (var prop in obj) {
+                if (count == random)
+                    return prop;
+
+                count++;
+            }
+        },
+        objLength: function(obj) {
+            var count = 0;
+
+            for (var prop in obj) {
+                count++;
+            }
+
+            return count;
         },
         reset: function() {
-            this.queue = 0;
-            this.chosen = {};
+            this.sectionCurrent = 0;
+            this.userChoices = [];
 
-            //$('.container').hide();
             $('#sounds').show();
 
             if (typeof PBAutoCommands != 'undefined') {
                 PBAutoCommands.moveSequenceToTime(false, 1, 0, 0, 0, 0);
                 PBAutoCommands.setSequenceTransportMode(false, 1, 'Stop');
             }
-
-            //console.log('Choosing: ' + this.selections[this.queue].selection);
         }
     };
 
     window.App = App;
 })(jQuery);
 
-/*
- (function($) {
- function Timer(delay) {
- this.delay = delay;
- 
- this.__construct();
- }
- 
- Timer.prototype = {
- __construct: function() {
- $.doTimeout(this.delay, function() {
- app.selection();
- });
- }
- };
- 
- window.Timer = Timer;
- })(jQuery);*/
+
+(function($) {
+    function Timer() {
+    }
+
+    Timer.prototype = {
+        start: function(delay, scope) {
+            $.doTimeout('timer', delay, function() {
+                scope.selection(scope);
+            });
+        }
+    };
+
+    window.Timer = Timer;
+})(jQuery);
